a0456146a71f04bc73fac3dc2637dc0cf7011e49
Jiwon <jiwon@vertigogames.com>
2021-06-11 23:54:49 -0700

    implement redux reducer action

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import Store from './redux/store';
 import App from './App';
+import { Provider } from 'react-redux';
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={Store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
