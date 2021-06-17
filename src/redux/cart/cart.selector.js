25e5f3a36b3c05f55ace4b0fb0f9e86f6d1fe3c5
Jiwon <jiwon@vertigogames.com>
2021-06-16 23:38:17 -0700

    add checkout page checkout item component updated cart to use selectors

--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -2,6 +2,11 @@ import { createSelector } from 'reselect';
 
 const selectCart = (state) => state.cart;
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
+
 export const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems
@@ -11,4 +16,11 @@ export const selectCartItemCount = createSelector(
   [selectCartItems],
   (cartItems) =>
     cartItems.reduce((acc, cartItems) => acc + cartItems.quantity, 0)
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (acc, cartItems) => acc + cartItems.quantity * cartItems.price,
+    0
+  )
 );
